f0dcfd121dcf0110b145f6c0602820e7583aba35
Mike Miller <mike.d.miller1@gmail.com>
2017-06-15 23:37:41 -0400

    Updating list of Routes when reading link file

--- src/RouterThread.java
+++ src/RouterThread.java
@@ -15,6 +15,7 @@ public class RouterThread extends Thread
 	protected String LinkFilename;
 	protected BufferedReader LinkFileReader = null;
 	protected boolean ServerRunning = true;
+	protected Node ThisRouter;
 	protected ArrayList<Link> Links = new ArrayList<Link>();
 	protected ArrayList<Route> Routes = new ArrayList<Route>();
 	public int BroadcastInterval;
@@ -36,18 +37,21 @@ public class RouterThread extends Thread
 	/*
 	 * Constructor
 	 */
-	public RouterThread( String name, int Port, String LinkFilename ) throws IOException
+	public RouterThread( String Name, int Port, String LinkFilename ) throws IOException
 	{
 		// Call the base Thread class constructor
-		super( name );
+		super( Name );
 
 		// Assign the link filename
 		this.LinkFilename = LinkFilename;
-
+		
 		// Create a new UDP socket
 		System.out.print("Starting UDP sender on port " + Port + "... ");
 		socket = new DatagramSocket(Port);
 		System.out.println("Done");
+		
+		// Create the node for this router
+		ThisRouter = new Node( Name, socket.getLocalAddress().toString(), Port );
 
 		// Read the link file
 		ReadLinkFile();
@@ -116,24 +120,27 @@ public class RouterThread extends Thread
 		BroadcastNumber++;
 		System.out.println("Broadcast number " + BroadcastNumber);
 		
+		// Read the link file
+		ReadLinkFile();
+		
 		// Serialize the list of routes
-		String RoutesString = FileUtilities.toString( Links );
+		String RoutesString = FileUtilities.toString( Routes );
 		
 		
-		// Loop through the routes
+		// Loop through the links
 		System.out.println( "Sending to " + Links.size() + " nodes" );
 		for( int RouteNum = 0; RouteNum < Links.size(); RouteNum++ )
 		{
-			// Get the current route
-			Link CurrentRoute = Links.get( RouteNum );
+			// Get the current link
+			Link CurrentLink = Links.get( RouteNum );
 			
-			// Send the link list to the current router
-			System.out.print( "Sending to " + CurrentRoute.Node.Name + " at " + CurrentRoute.Node.IpAddress + ":" + CurrentRoute.Node.Port + "... " );
+			// Send the route list to the current link
+			System.out.print( "Sending to " + CurrentLink.Node.Name + " at " + CurrentLink.Node.IpAddress + ":" + CurrentLink.Node.Port + "... " );
 			DatagramPacket packet = new DatagramPacket(
 					RoutesString.getBytes(),
 					RoutesString.length(),
-					InetAddress.getByName( CurrentRoute.Node.IpAddress ),
-					CurrentRoute.Node.Port );
+					InetAddress.getByName( CurrentLink.Node.IpAddress ),
+					CurrentLink.Node.Port );
 			socket.send( packet );
 			System.out.println( "Done" );
 		}
@@ -144,7 +151,7 @@ public class RouterThread extends Thread
 	
 	
 	/*
-	 * Reads the link file and updates the list of links.
+	 * Reads the link file and updates the list of links and routes.
 	 */
 	protected void ReadLinkFile() throws IOException
 	{
@@ -156,7 +163,7 @@ public class RouterThread extends Thread
 		// Open the file reader
 		try
 		{
-			LinkFileReader = new BufferedReader(new FileReader(LinkFilename));
+			LinkFileReader = new BufferedReader( new FileReader(LinkFilename) );
 		}
 
 		catch( FileNotFoundException e )
@@ -164,17 +171,56 @@ public class RouterThread extends Thread
 			System.err.println("Could not open link file.");
 		}
 
+		
 		// Get the number of links
-		int NumberOfLinks = Integer.parseInt(LinkFileReader.readLine());
+		int NumberOfLinks = Integer.parseInt( LinkFileReader.readLine() );
 
 		// Loop through the links
 		for( int LinkNum = 0; LinkNum < NumberOfLinks; LinkNum++ )
 		{
-			// Create a new link and add it to the list
-			Links.add(new Link(LinkFileReader.readLine()));
+			// Create a new link and add it to the list of links
+			Link NewLink = new Link( LinkFileReader.readLine() );
+			Links.add( NewLink );
+			
+			// Check if there is already a route to the link
+			Route CurrentRoute = FindRoute( NewLink.Node.Name );
+			
+			// If there is, update the route
+			if( CurrentRoute != null )
+			{
+				CurrentRoute.Cost = NewLink.Cost;
+			}
+			
+			// Otherwise, create a new route
+			else
+			{
+				Routes.add( new Route( ThisRouter, NewLink.Node, NewLink.Node, NewLink.Cost ) );
+			}
 		}
 
 		System.out.println("Done");
 		System.out.println("Found " + NumberOfLinks + " links");
+	}
+	
+	
+	
+	/*
+	 * Returns the route to the specified destination router name.
+	 * If no route is found, returns null.
+	 */
+	Route FindRoute( String Name )
+	{
+		// Loop through the routes
+		for( int RouteNum = 0; RouteNum < Routes.size(); RouteNum++ )
+		{
+			// If the current route destination name matches the search name, return the route
+			if( Routes.get( RouteNum ).Destination.Name.equals( Name ) )
+			{
+				return Routes.get( RouteNum );
+			}
+		}
+		
+		// If we get here, the route was not found
+		return null;
 	}
 }
